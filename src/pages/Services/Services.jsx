eda80ecfb884277d15c6fa24e9fc059a4b89c7f5
mdmuzahid07 <mdmuzahid7396@gmail.com>
2022-07-17 03:22:11 +0600

    working on services card

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Services_card from './Services_card';
 
 const Services = () => {
     return (
         <div>
-            <h2>service page</h2>
+            <section className='bg-slate-700 rounded-t-lg my-7 h-44 flex justify-center items-center'>
+                <h2 className='md:text-5xl font-bold text-white text-center'>Our Services</h2>
+            </section>
+            <Services_card />
         </div>
     );
 };
---
+++ src/pages/Services/Services_card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Services_card = () => {
+    return (
+        <div className='mt-10'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-3'>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services_card;
