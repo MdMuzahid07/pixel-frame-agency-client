7f96fb29cbc819fdd8b632e1b68e65e1123d6145
mdmuzahid07 <mdmuzahid7396@gmail.com>
2022-07-17 16:18:59 +0600

    skill section designn complete

--- src/pages/AboutUs/Our_Skills.jsx
+++ src/pages/AboutUs/Our_Skills.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Our_Skills = () => {
     return (
         <div className='grid lg:grid-cols-2 my-20'>
-            <div>
-
-            </div>
+            <img className='rounded-xl p-4' src="https://i.ibb.co/kXpkgvJ/Untitled-design.png" alt="" />
             <div>
                 <div>
                     <h2>{`<> UI/UX </>`}</h2>
@@ -13,26 +11,26 @@ const Our_Skills = () => {
                 </div>
                 <div className='my-4'>
                     <h2>{`<> Android Development </>`}</h2>
-                    <progress class="progress progress-success bg-slate-900" value="50" max="100"></progress>
+                    <progress class="progress progress-success bg-slate-900" value="70" max="100"></progress>
                 </div>
                 <div>
                     <h2>{`<> IOS Development </>`}</h2>
-                    <progress class="progress progress-success bg-slate-900" value="50" max="100"></progress>
+                    <progress class="progress progress-success bg-slate-900" value="75" max="100"></progress>
                 </div>
                 <div className='my-4'>
                     <h2>{`<> Web Development </>`}</h2>
-                    <progress class="progress progress-success bg-slate-900" value="50" max="100"></progress>
+                    <progress class="progress progress-success bg-slate-900" value="90" max="100"></progress>
                 </div>
                 <div>
                     <h2>{`<> Digital Marketing </>`}</h2>
-                    <progress class="progress progress-success bg-slate-900" value="50" max="100"></progress>
+                    <progress class="progress progress-success bg-slate-900" value="70" max="100"></progress>
                 </div>
                 <div className='mt-4'>
                     <h2>{`<> Product Design </>`}</h2>
-                    <progress class="progress progress-success bg-slate-900" value="50" max="100"></progress>
+                    <progress class="progress progress-success bg-slate-900" value="90" max="100"></progress>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
